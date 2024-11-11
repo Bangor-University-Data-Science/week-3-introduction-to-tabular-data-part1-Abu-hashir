d772b2b1894a033966cba7c3e3d5241c2bf4e7ce
Zohaib Ali <zohaibali51510@gmail.com>
2024-11-11 13:38:48 +0000

    Lab 3 Complete

--- titanic_analysis/data_loader.py
+++ titanic_analysis/data_loader.py
@@ -3,4 +3,5 @@ import pandas as pd
 
 def load_titanic_data(filepath: str) -> pd.DataFrame:
     """Loads the Titanic dataset from a CSV file."""
-    return pd.read_csv(filepath)
+    df = pd.read_csv('data/titanic.csv')
+    return df
